59a807215f7f0b0f5b770068797569c424cec281
Ayush Katara <ayush.katara@quovantis.com>
2017-03-09 16:29:04 +0530

    serializer edit

--- votr/teams/serializers.py
+++ votr/teams/serializers.py
@@ -1,22 +1,37 @@
 from rest_framework import serializers
 
-from teams.models import Teams
+from teams.models import Teams, Members
+
+
+class MemberInfo(serializers.ModelSerializer):
+
+    class Meta:
+        model = Members
+        fields = '__all__'
+
+
+class TeamInfo(serializers.ModelSerializer):
+
+    class Meta:
+        model = Teams
+        fields = ('team_name', 'company')
 
 
 class TeamSerializer(serializers.Serializer):
 
     company = serializers.CharField(required=True, max_length=50)
     team_name = serializers.CharField(required=True, max_length=50)
+    member = MemberInfo(required=False)
 
 
 class MemberSerializer(serializers.Serializer):
 
     name = serializers.CharField(required=True, max_length=50)
     age = serializers.IntegerField()
-    team_name = serializers.CharField(required=True, max_length=50)
-    company = serializers.CharField(required=True, max_length=50)
+    team = TeamInfo(required=True)
 
     def validate(self, data):
 
-        if not Teams.objects.filter(team_name=data.get("team_name")).exists():
+        self.team = data.get("team")
+        if not Teams.objects.filter(team_name=self.team.get("team_name")).exists():
             raise serializers.ValidationError("Invalid Team")
